65a4ca86acace0938a658f0367152013540439da
martingonzalezr001 <martinglezrodriguez@gmail.com>
2025-02-24 21:39:56 +0100

    Api Implementada y avances en streamlit

--- front/front-end.py
+++ front/front-end.py
@@ -17,27 +17,29 @@ with st.form("my_form"):
 
     submitted = st.form_submit_button(label="Enviar", help=None, on_click=None, args=None, kwargs=None, type="secondary", icon=None, disabled=False, use_container_width=False)
 
-    # Every form must have a submit button.
-    
-    if submitted:
-        st.write(idea)
-    
-# Bloque Front
+    parameter_list = []
+
+    # Bloque Front
 checkbox_front = st.checkbox("Front-End", key="front-End")
 if checkbox_front:
 
     # Crear columnas
     col_escalabilidad_front, col_nivel_uso_front, col_coste_front, col_seguridad_front = st.columns(4)
     with col_escalabilidad_front:
-        escalabilidad_front = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_front")
+        escalabilidad_front = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_front")
     with col_nivel_uso_front:
-        nivel_uso_front = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_front")
+        nivel_uso_front = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_front")
 
     with col_coste_front:
-        coste_front = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_front")
+        coste_front = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_front")
 
     with col_seguridad_front:
-        seguridad_front = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_front") 
+        seguridad_front = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_front")
+    
+    
+    front_list = [{"Escalabilidad":escalabilidad_front},{ "Nivel de Uso":nivel_uso_front}, {"Coste":coste_front}, {"Seguridad":seguridad_front}] 
+    parameter_list.append(front_list)
+
 
 # Bloque Back
 checkbox_back = st.checkbox("Back-End", key="back-End") 
@@ -47,16 +49,18 @@ if checkbox_back:
 
     # Crear cada columna con su select
     with col_escalabilidad_back:
-        escalabilidad_back = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_back")
+        escalabilidad_back = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_back")
     with col_nivel_uso_back:
-        nivel_uso_back = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_back")
+        nivel_uso_back = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_back")
 
     with col_coste_back:
-        coste_back = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_back")
+        coste_back = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_back")
 
     with col_seguridad_back:
-        seguridad_back = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_back")   
+        seguridad_back = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_back")
 
+    back_list = [{"Escalabilidad":escalabilidad_back},{ "Nivel de Uso":nivel_uso_back}, {"Coste":coste_back}, {"Seguridad":seguridad_back}] 
+    parameter_list.append(back_list)
 
 # Bloque Database
 checkbox_database = st.checkbox("Database", key="database")
@@ -66,16 +70,18 @@ if checkbox_database:
 
     # Crear cada columna con su select
     with col_escalabilidad_database:
-        escalabilidad_database = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_database")
+        escalabilidad_database = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_database")
     with col_nivel_uso_database:
-        nivel_uso_database = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_database")
+        nivel_uso_database = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_database")
 
     with col_coste_database:
-        coste_database = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_database")
+        coste_database = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_database")
 
     with col_seguridad_database:
-        seguridad_database = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_database")  
+        seguridad_database = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_database")
 
+    database_list = [{"Escalabilidad":escalabilidad_database},{ "Nivel de Uso":nivel_uso_database}, {"Coste":coste_database}, {"Seguridad":seguridad_database}]  
+    parameter_list.append(database_list)
 
 # Bloque Authentication
 checkbox_auth = st.checkbox("Authentication", key="authentication")
@@ -85,16 +91,18 @@ if checkbox_auth:
 
     # Crear cada columna con su select
     with col_escalabilidad_auth:
-        escalabilidad_auth = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_auth")
+        escalabilidad_auth = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_auth")
     with col_nivel_uso_auth:
-        nivel_uso_auth = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_auth")
+        nivel_uso_auth = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_auth")
 
     with col_coste_auth:
-        coste_auth = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_auth")
+        coste_auth = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_auth")
 
     with col_seguridad_auth:
-        seguridad_auth = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_auth")
+        seguridad_auth = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_auth")
 
+    auth_list = [{"Escalabilidad":escalabilidad_auth},{ "Nivel de Uso":nivel_uso_auth}, {"Coste":coste_auth}, {"Seguridad":seguridad_auth}]
+    parameter_list.append(auth_list)
 
 # Bloque E-commerce
 checkbox_ecommerce = st.checkbox("E-Commerce", key="e-Commerce")
@@ -104,16 +112,18 @@ if checkbox_ecommerce:
 
     # Crear cada columna con su select
     with col_escalabilidad_ecommerce:
-        escalabilidad_ecommerce = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_ecommerce")
+        escalabilidad_ecommerce = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_ecommerce")
     with col_nivel_uso_ecommerce:
-        nivel_uso_ecommerce = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_ecommerce")
+        nivel_uso_ecommerce = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_ecommerce")
 
     with col_coste_ecommerce:
-        coste_ecommerce = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_ecommerce")
+        coste_ecommerce = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_ecommerce")
 
     with col_seguridad_ecommerce:
-        seguridad_ecommerce = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_ecommerce")
+        seguridad_ecommerce = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_ecommerce")
 
+    ecommerce_list = [{"Escalabilidad":escalabilidad_ecommerce},{ "Nivel de Uso":nivel_uso_ecommerce}, {"Coste":coste_ecommerce}, {"Seguridad":seguridad_ecommerce}]
+    parameter_list.append(ecommerce_list)
 
 # Bloque CloudHosting
 checkbox_cloud_hosting = st.checkbox("CloudHosting", key="cloud_hosting")
@@ -123,15 +133,18 @@ if checkbox_cloud_hosting:
 
     # Crear cada columna con su select
     with col_escalabilidad_cloud_hosting:
-        escalabilidad_cloud_hosting = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_cloud_hosting")
+        escalabilidad_cloud_hosting = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_cloud_hosting")
     with col_nivel_uso_cloud_hosting:
-        nivel_uso_cloud_hosting = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_cloud_hosting")
+        nivel_uso_cloud_hosting = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_cloud_hosting")
 
     with col_coste_cloud_hosting:
-        coste_cloud_hosting = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_cloud_hosting")
+        coste_cloud_hosting = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_cloud_hosting")
 
     with col_seguridad_cloud_hosting:
-        seguridad_cloud_hosting = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_cloud_hosting")
+        seguridad_cloud_hosting = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_cloud_hosting")
+
+    cloud_hosting_list = [{"Escalabilidad":escalabilidad_cloud_hosting},{ "Nivel de Uso":nivel_uso_cloud_hosting}, {"Coste":coste_cloud_hosting}, {"Seguridad":seguridad_cloud_hosting}]
+    parameter_list.append(cloud_hosting_list)
 
 # Bloque CD/CI & DevOps
 checkbox_cdci_devops = st.checkbox("CD/CI & DevOps", key="cd/ci_devOps")
@@ -141,15 +154,18 @@ if checkbox_cdci_devops:
 
     # Crear cada columna con su select
     with col_escalabilidad_cdci_devops:
-        escalabilidad_cdci_devops = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_cdci_devops")
+        escalabilidad_cdci_devops = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_cdci_devops")
     with col_nivel_uso_cdci_devops:
-        nivel_uso_cdci_devops = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_cdci_devops")
+        nivel_uso_cdci_devops = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_cdci_devops")
 
     with col_coste_cdci_devops:
-        coste_cdci_devops = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_cdci_devops")
+        coste_cdci_devops = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_cdci_devops")
 
     with col_seguridad_cdci_devops:
-        seguridad_ecommerce = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_cdci_devops") 
+        seguridad_cdci_devops = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_cdci_devops")
+
+    cdci_devops_list = [{"Escalabilidad":escalabilidad_cdci_devops},{ "Nivel de Uso":nivel_uso_cdci_devops}, {"Coste":coste_cdci_devops}, {"Seguridad":seguridad_cdci_devops}]
+    parameter_list.append(cdci_devops_list)
 
 # Bloque Message Service
 checkbox_msg = st.checkbox("Message Service", key="message_service")
@@ -159,15 +175,19 @@ if checkbox_msg:
 
     # Crear cada columna con su select
     with col_escalabilidad_msg:
-        escalabilidad_msg = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_msg")
+        escalabilidad_msg = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_msg")
+    
     with col_nivel_uso_msg:
-        nivel_uso_msg = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_msg")
+        nivel_uso_msg = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_msg")
 
     with col_coste_msg:
-        coste_msg = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_msg")
+        coste_msg = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_msg")
 
     with col_seguridad_msg:
-        seguridad_msg = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_msg")
+        seguridad_msg = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_msg")
+    
+    msg_list = [{"Escalabilidad":escalabilidad_msg},{ "Nivel de Uso":nivel_uso_msg}, {"Coste":coste_msg}, {"Seguridad":seguridad_msg}]
+    parameter_list.append(msg_list)
 
 # Bloque Search Engine
 checkbox_search_engine = st.checkbox("Search Engine", key="search_engine") 
@@ -177,15 +197,18 @@ if checkbox_search_engine:
 
     # Crear cada columna con su select
     with col_escalabilidad_search_engine:
-        escalabilidad_search_engine = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_search_engine")
+        escalabilidad_search_engine = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_search_engine")
     with col_nivel_uso_search_engine:
-        nivel_uso_search_engine = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_search_engine")
+        nivel_uso_search_engine = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_search_engine")
 
     with col_coste_search_engine:
-        coste_search_engine = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_search_engine")
+        coste_search_engine = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium",], key="coste_search_engine")
 
     with col_seguridad_search_engine:
-        seguridad_search_engine = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_search_engine")
+        seguridad_search_engine = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_search_engine")
+
+    search_engine_list = [{"Escalabilidad":escalabilidad_search_engine},{ "Nivel de Uso":nivel_uso_search_engine}, {"Coste":coste_search_engine}, {"Seguridad":seguridad_search_engine}]
+    parameter_list.append(search_engine_list)
 
 # Bloque media_storage
 
@@ -196,12 +219,26 @@ if checkbox_media_storage:
 
     # Crear cada columna con su select
     with col_escalabilidad_media_storage:
-        escalabilidad_media_storage = st.selectbox("Escalabilidad:", ["Alta", "Media", "Baja","N/A"], key="escalabilidad_media_storage")
+        escalabilidad_media_storage = st.selectbox("Escalabilidad:", ["N/A", "Alta", "Media", "Baja"], key="escalabilidad_media_storage")
     with col_nivel_uso_media_storage:
-        nivel_uso_media_storage = st.selectbox("Nivel de uso:", ["Sencillo", "Intermedio", "Difícil", "N/A"], key="nivel_uso_media_storage")
+        nivel_uso_media_storage = st.selectbox("Nivel de uso:", ["N/A", "Sencillo", "Intermedio", "Difícil"], key="nivel_uso_media_storage")
 
     with col_coste_media_storage:
-        coste_media_storage = st.selectbox("Coste:", ["Gratuito", "De pago", "Freemium", "N/A"], key="coste_media_storage")
+        coste_media_storage = st.selectbox("Coste:", ["N/A", "Gratuito", "De pago", "Freemium"], key="coste_media_storage")
 
     with col_seguridad_media_storage:
-        seguridad_media_storage = st.selectbox("Seguridad:", ["Alta", "Media", "Básica", "N/A"], key="seguridad_media_storage")
+        seguridad_media_storage = st.selectbox("Seguridad:", ["N/A", "Alta", "Media", "Básica"], key="seguridad_media_storage")
+
+    media_storage_list = [{"Escalabilidad":escalabilidad_media_storage},{ "Nivel de Uso":nivel_uso_media_storage}, {"Coste":coste_media_storage}, {"Seguridad":seguridad_media_storage}]
+    parameter_list.append(media_storage_list)
+
+
+    # Every form must have a submit button.
+    
+if submitted:
+    st.write(idea)
+    for x in parameter_list:
+        st.write(x)
+         
+
+    
